1edba5745cc87a22c9a4a2c00712dc9a9ddfdd9b
buuing <17778888138@189.cn>
2021-01-26 21:32:37 +0800

    feature: 增加大转盘抽奖

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { name } from './package.json'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: './src/index.js',
+  output: [
+    {
+      file: `dist/${name}.cjs.js`,
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    babel(),
+    resolve(),
+    commonjs(),
+    json(),
+  ],
+  external: ['react'],
+}
---
+++ src/components/LuckyWheel.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { LuckyWheel as Wheel } from 'lucky-canvas'
+
+export default class LuckyWheel extends React.Component {
+  constructor (props) {
+    super(props)
+    this.myLucky = React.createRef()
+  }
+  componentDidMount () {
+    const { props } = this
+    try {
+      this.init()
+      props.onSuccess && props.onSuccess()
+    } catch (err) {
+      props.onError && props.onError(err)
+    } finally {
+      props.onFinally && props.onFinally(err)
+    }
+  }
+  init () {
+    const { props } = this
+    this.$lucky = new Wheel({
+      flag: 'WEB',
+      width: props.width,
+      height: props.height,
+      divElement: this.myLucky.current
+    }, {
+      ...props,
+      start: (...rest) => {
+        props.onStart && props.onStart(...rest)
+      },
+      end: (...rest) => {
+        props.onEnd && props.onEnd(...rest)
+      }
+    })
+  }
+  play (...rest) {
+    this.$lucky.play(...rest)
+  }
+  stop (...rest) {
+    this.$lucky.stop(...rest)
+  }
+  render () {
+    return <div ref={this.myLucky}></div>
+  }
+}
+
+LuckyWheel.defaultProps = {
+  width: '',
+  height: '',
+  prizes: [],
+  blocks: [],
+  buttons: [],
+  defaultStyle: {},
+  defaultConfig: {},
+}
